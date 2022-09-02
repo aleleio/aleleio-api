39aa65a7d75fe7f491bf9433a2598241bfeed97a
Christian Macht <christian.macht@gmail.com>
2022-09-02 11:18:25 +0200

    add services.github

--- src/services/github.py
+++ src/services/github.py
@@ -1,11 +1,18 @@
+"""
+Use GitHub Apps to automatically push changes in the game database to the TB Games repository.
+
+Personal Access Tokens are tied to user accounts and enable full access, while GitHub Apps allows granular permissions.
+
+Helpful:
+- https://medium.com/@gilharomri/github-app-bot-with-python-ea38811d7b14
+- https://docs.github.com/en/rest/guides/getting-started-with-the-rest-api
+"""
 def push_multiple_games(games):
     for game in games:
         push_single_game(game)
 
 
 def push_single_game(game):
-    """https://docs.github.com/en/rest/guides/getting-started-with-the-rest-api
-    """
     # export_to_markdown()
     # connect & push
     pass
